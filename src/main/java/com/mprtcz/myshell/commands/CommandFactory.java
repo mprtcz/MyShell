3331d9ce1c0507159a3c44b5f710744511fdf3a5
Mordin <azet89@gmail.com>
2016-07-20 01:37:06 +0200

    assigned CommandFactory constants

--- src/main/java/com/mprtcz/myshell/commands/CommandFactory.java
+++ src/main/java/com/mprtcz/myshell/commands/CommandFactory.java
@@ -6,18 +6,25 @@ import com.mprtcz.myshell.utils.Arguments;
  * Created by Azet on 2016-07-19.
  */
 public class CommandFactory {
+    private static final String CD = "cd";
+    private static final String PROMPT = "prompt";
+    private static final String DIR = "dir";
+    private static final String EXIT = "exit";
+    private static final String TREE = "tree";
+
+
     public static Command getCommand(Arguments arguments) {
         Command command = null;
 
-        if (arguments.getCommand().equals("cd")) {
+        if (arguments.getCommand().equals(CD)) {
             command = new Cd(arguments.getParameter());
-        } else if (arguments.getCommand().equals("prompt")) {
+        } else if (arguments.getCommand().equals(PROMPT)) {
             command = new Prompt(arguments.getParameter());
-        } else if (arguments.getCommand().equals("dir")) {
+        } else if (arguments.getCommand().equals(DIR)) {
             command = new Dir();
-        } else if (arguments.getCommand().equals("exit")) {
+        } else if (arguments.getCommand().equals(EXIT)) {
             command = new Exit();
-        } else if (arguments.getCommand().equals("tree")) {
+        } else if (arguments.getCommand().equals(TREE)) {
             command = new Tree();
         } else {
             command = new UnknownCommand(arguments.getCommand());
