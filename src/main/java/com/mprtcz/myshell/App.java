67c0d7586e09d813d1f46924c7fa69b1dd3405c9
Mordin <azet89@gmail.com>
2016-07-19 09:41:47 +0200

    commit adding files to the repo

---
+++ src/main/java/com/mprtcz/myshell/App.java
@@ -0,0 +1,15 @@
+package com.mprtcz.myshell;
+
+/**
+ * Created by Azet on 2016-07-18.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        MyShell myShell = new MyShell();
+        do {
+            myShell.getCommand();
+        } while (myShell.isRunning());
+    }
+
+}
---
+++ src/main/java/com/mprtcz/myshell/MyShell.java
@@ -0,0 +1,111 @@
+package com.mprtcz.myshell;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by Azet on 2016-07-19.
+ */
+public class MyShell {
+    private String prompt = "$>";
+    private boolean running = true;
+    private File currentDirectory = new File("");
+
+    public MyShell() {
+        currentDirectory = new File(getWorkingDirectory());
+    }
+
+    String getDirectories(File filepath) {
+        File file = new File("/");
+        String[] directories = file.list((current, name) -> new File(current, name).isDirectory());
+        System.out.println(Arrays.toString(directories));
+        return null;
+    }
+
+    void getCommand() {
+        displayString();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        recognizeCommand(input);
+    }
+
+    private void recognizeCommand(String input) {
+        if (!input.equals("")) {
+            System.out.println(input);
+            String command = input.split(" ")[0];
+            switch (command) {
+                case "prompt":
+                    prompt = input.split(" ")[1] + ">";
+                    break;
+                case "reset":
+                    prompt = "$>";
+                    break;
+                case "exit":
+                    running = false;
+                    break;
+                case "$cwd":
+                    displayString(getWorkingDirectory());
+                    break;
+                case "dir":
+                    displayString(displayCurrentDirectoryContents());
+                    break;
+                default:
+                    System.out.println("Command not recognized");
+                    break;
+            }
+        }
+    }
+
+    boolean isRunning() {
+        return running;
+    }
+
+    private String getWorkingDirectory() {
+        return currentDirectory.getAbsolutePath();
+    }
+
+    private void displayString(String string) {
+        String toDisplay = "[MyShell] " +
+                prompt +
+                string;
+        System.out.print(toDisplay);
+        recognizeCommand(getNoPromptCommand());
+    }
+
+    private void displayString() {
+        String toDisplay = "[MyShell] " +
+                prompt;
+        System.out.print(toDisplay);
+    }
+
+    private String getNoPromptCommand() {
+        System.out.print(">");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private String displayCurrentDirectoryContents() {
+        ArrayList<File> files = new ArrayList<>(Arrays.asList(currentDirectory.listFiles()));
+        return recognizeFolderContents(files);
+    }
+
+    String recognizeFolderContents(ArrayList<File> contents){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("\n");
+
+        for(File f : contents){
+            if(f.isDirectory()){
+                stringBuilder.append("DIR " +f.getName());
+                stringBuilder.append("\n");
+            } else {
+                stringBuilder.append("FILE " +f.getName());
+                stringBuilder.append("\n");
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+}
